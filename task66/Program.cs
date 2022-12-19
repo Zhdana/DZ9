30346607e22cda3eaf5f1601dcf706b253f96061
Zhdana <zhdanasulina@yandex.ru>
2022-12-19 10:13:43 +0300

    first commit

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите числа M и N");
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(M,N));
+int Sum(int m, int n)
+{
+    if(m==n)
+    {
+        return m;
+    }
+    m++;   
+    return m+Sum(m,n);
+}
+
---
+++ task68/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу вычесления функций Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.WriteLine("Введите числа M и N");
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akkerman(M,N));
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
